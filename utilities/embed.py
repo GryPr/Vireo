899c8b0a0015f5733684ae193390e407e0bde2a0
Kevin <promkev@gmail.com>
2022-03-30 19:44:21 -0400

    Added command to check portal ID and added handling when trying to subscribe a channel that is already subscribed to a portal

--- utilities/embed.py
+++ utilities/embed.py
@@ -2,9 +2,10 @@ import disnake
 from disnake import Embed
 
 
-def generic_embed(title: str, author: str) -> Embed:
+def generic_embed(title: str, description: str, author: str) -> Embed:
     embed = disnake.Embed(color=0x09a2e3)
     embed.title = title
+    embed.description = description
     embed.set_footer(
         text=f"Requested by {author}"
     )
@@ -15,8 +16,9 @@ def wip_embed() -> Embed:
     embed = disnake.Embed(title="You're early!", description=f"Command has not been implemented yet!")
     return embed
 
-def error_embed(error: str) -> Embed:
+
+def error_embed(error: str, title: str = "Error has occurred") -> Embed:
     embed = disnake.Embed(color=0xff0000)
-    embed.title = "Error has occured"
+    embed.title = title
     embed.description = error
     return embed
