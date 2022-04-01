21e1eadace225e0001b330845cea5089e8b3a6cb
Kevin Prom <promkev@gmail.com>
2022-04-01 12:05:56 -0400

    Removed test guilds parameters. Slash commands now take one hour to propagate, but will now propagate to all servers.

--- main.py
+++ main.py
@@ -57,8 +57,7 @@ bot = Bot(command_prefix=os.environ.get("PREFIX", default="v!"),
           intents=disnake.Intents.all(),
           help_command=None,  # type: ignore
           sync_commands_debug=True,
-          sync_permissions=True,
-          test_guilds=[956366437532971068, 927670915423158322, 751142001147248843, 808501710460289025])
+          sync_permissions=True)
 
 
 @bot.event
