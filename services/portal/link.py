e4b10d554587fb2ad5472ce98d9255f2beb60726
volovikariel <16262535+volovikariel@users.noreply.github.com>
2022-05-17 18:38:18 -0400

    refactor: standardized docstrings
    Made single line doctstrings span a single line.

--- services/portal/link.py
+++ services/portal/link.py
@@ -1,25 +1,20 @@
 import disnake
-from disnake import WebhookMessage, RawMessageUpdateEvent
-from disnake.utils import MISSING
-
 from components.MessageReplyView import MessageReplyView
+from disnake import RawMessageUpdateEvent, WebhookMessage
+from disnake.utils import MISSING
 from services.database.message_db import add_message, retrieve_message_to_reply
 from services.portal.webhook import Webhook
 from utilities.filter import filter_words
 
 
 class Link:
-    """
-    A class that bundles a webhook and a channel. Multiple "links" combine to form a "chain".
-    """
+    """A class that bundles a webhook and a channel. Multiple "links" combine to form a "chain"."""
     hook: disnake.Webhook
     channel: disnake.TextChannel
 
     @classmethod
     async def new(cls, channel: disnake.TextChannel):
-        """
-        Create a new Link object.
-        """
+        """Create a new Link object."""
         self = cls()
         self.channel = channel
         self.hook = await Webhook.connect(channel, "Vireo")
