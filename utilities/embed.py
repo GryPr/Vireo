d26a24aefc501a563613b9dc1a27f45feddb2e69
volovikariel <16262535+volovikariel@users.noreply.github.com>
2022-05-17 22:46:11 -0400

    refactor: minor cleanup for utilities/embed.py

--- utilities/embed.py
+++ utilities/embed.py
@@ -1,8 +1,7 @@
 import disnake
-from disnake import Embed
 
 
-def generic_embed(title: str, description: str, author: str) -> Embed:
+def generic_embed(title: str, description: str, author: str) -> disnake.Embed:
     embed = disnake.Embed(color=0x09a2e3)
     embed.title = title
     embed.description = description
@@ -10,13 +9,13 @@ def generic_embed(title: str, description: str, author: str) -> Embed:
     return embed
 
 
-def wip_embed() -> Embed:
+def wip_embed() -> disnake.Embed:
     embed = disnake.Embed(title="You're early!",
-                          description=f"Command has not been implemented yet!")
+                          description="Command has not been implemented yet!")
     return embed
 
 
-def error_embed(error: str, title: str = "Error has occurred") -> Embed:
+def error_embed(error: str, title: str = "Error has occurred") -> disnake.Embed:
     embed = disnake.Embed(color=0xff0000)
     embed.title = title
     embed.description = error
